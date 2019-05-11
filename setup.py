fb40ccf3d3d40da9ab207d9f5a693b38f5871bd9
Sathyapriyasathish <50510064+Sathyapriyasathish@users.noreply.github.com>
2019-05-11 10:37:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,3 +22,45 @@ setup(
     url='https://github.com/IBM-Bluemix/python-hello-world-flask',
     license='Apache-2.0'
 )
+import random
+
+number = random.randint(1, 9)
+
+guess = 0
+
+count = 0
+
+
+
+while guess != number and guess != "exit":
+
+    guess = input("What's your guess?")
+
+
+
+    if guess == "exit":
+
+        break
+
+
+
+    guess = int(guess)
+
+    count += 1
+
+
+
+    if guess < number:
+
+        print("Too low!")
+
+    elif guess > number:
+
+        print("Too high!")
+
+    else:
+
+        print("You got it!")
+
+        print("And it only took you", count, "tries!")
+ 
